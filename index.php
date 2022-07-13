249154c1d973bb864ab08b76ae70560b4411230f
Darlan444 <darlaninacio321@gmail.com>
2022-07-13 03:56:16 -0300

    img responsive

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="./css/style.css"/>
+
 <?php
 include 'conexao.php';
 
@@ -8,9 +8,25 @@ $sqlimg = "SELECT * FROM img";
 $result = $mysql_db->query($sqlimg);
 while ($data = mysqli_fetch_assoc($result)) {
     echo '
-        <div class="portfolio-item "> 
+        <div class="img"> 
+            <h1 class="h1">'.$data['titulo'].'</h1>
             <img class="imagens" src="./img/' . $data['nome'] . '" alt="Imagem">
         </div>
     ';
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INDEX</title>
+
+    <link rel="stylesheet" href="./css/style.css"/>
+</head>
+<body>
+    
+</body>
+</html>
