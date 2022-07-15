d9d4ad9a82a9505499fc89b5f2def5bae4f34cdd
Darlan444 <darlaninacio321@gmail.com>
2022-07-15 03:26:33 -0300

    responsive

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/foguete.svg" type="image/x-icon">
+    <title>SPACE</title>
 
+    <link rel="stylesheet" href="./css/style.css"/>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
-include 'conexao.php';
+include './config/conexao.php';
 
 $row = array();
 
@@ -16,17 +30,4 @@ while ($data = mysqli_fetch_assoc($result)) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INDEX</title>
 
-    <link rel="stylesheet" href="./css/style.css"/>
-</head>
-<body>
-    
-</body>
-</html>
--- view/form.php
+++ view/form.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
 
+    <link rel="shortcut icon" href="../img/foguete.svg" type="image/x-icon">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <link rel="stylesheet" href="../css/style.css">
